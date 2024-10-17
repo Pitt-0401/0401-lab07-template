94d9cd29dad65cb89e3e76aa3b03c53cffba1bd6
Nadine von Frankenberg <vonfrankenberg@pitt.edu>
2024-10-16 20:04:21 -0400

    Add initial draft

---
+++ src/BinarySearch.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 2024-10-16 
+ *
+ * Copyright (c) 2024 Nadine von Frankenberg
+ */
+
+public class BinarySearch {
+
+    public static int searchForCat(Cat[] array, int key) {
+        // TODO: Task 2 - implement binary search to search for a key (= age)
+        // Return the cat's index
+        return -1;
+    }
+}
